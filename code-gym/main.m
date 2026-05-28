2dca4a60e8ef8fc84ad91550b6bd49bdc8b9e96b
Eder Carmona <vce00377@innovaccion.mx>
2026-05-28 01:05:48 -0600

    Initial Commit

---
+++ code-gym/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  code-gym
+//
+//  Created by Eder Carmona Armijo on 28/05/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
