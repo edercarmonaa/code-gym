65d5afffad61293b83a4897a67628696a063dcc5
Eder Carmona <vce00377@innovaccion.mx>
2026-06-12 12:02:41 -0600

    mas ajecicios

--- objective-c/code-gym/Ejercicio002.m
+++ objective-c/code-gym/Ejercicio002.m
@@ -9,18 +9,35 @@
 #import "Ejercicios.h"
 
 @implementation Ejercicio002
-BOOL esAnagrama(NSString *palabra1, NSString *palabra2){
-    NSUInteger longitud1 = [palabra1 length];
-    NSUInteger longitud2 = [palabra2 length];
-    if(longitud1== longitud2){
-        return(true);
-    }else{
-        return (false);
+BOOL esAnagrama(NSString *cadena1, NSString *cadena2){
+    NSString *cadenaLimpia1 = [cadena1 stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSString *cadenaLimpia2 = [cadena2 stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSUInteger longitud1 = [cadenaLimpia1 length];
+    NSUInteger longitud2 = [cadenaLimpia2 length];
+    
+    NSCountedSet *set1 = [[NSCountedSet alloc] init];
+    NSCountedSet *set2 = [[NSCountedSet alloc] init];
+    
+    if(longitud1 != longitud2){
+        return(false);
     }
+    
+    // Llenar el primer set con los caracteres de cadena1
+    for (NSUInteger i = 0; i < longitud1; i++) {
+        [set1 addObject:@([cadenaLimpia1 characterAtIndex:i])];
+    }
+    
+    // Llenar el segundo set con los caracteres de cadena2
+    for (NSUInteger i = 0; i < longitud2; i++) {
+        [set2 addObject:@([cadenaLimpia2 characterAtIndex:i])];
+    }
+    
+    // Comparar si ambos sets tienen los mismos elementos y frecuencias
+    return [set1 isEqualToSet:set2];
 }
 
 + (void)ejecutar {
-    BOOL resultado = esAnagrama(@"eder", @"rede");
+    BOOL resultado = esAnagrama(@"e de r", @"rede");
     NSLog(@"%d", resultado);
 }
 
