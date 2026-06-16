f0fe2eae839bc1e286b40812010a292591021d44
Eder Carmona <vce00377@innovaccion.mx>
2026-06-16 11:32:05 -0600

    solution005. java

---
+++ java/Solution005.java
@@ -0,0 +1,11 @@
+/*
+Dado un string text y un string prefix, devuelve true si text comienza exactamente
+ con prefix, o false en caso contrario.
+
+La comparación es sensible a mayúsculas y minúsculas.
+*/
+public class Solution005 {
+    public boolean startsWith(String text, String prefix) {
+        return text.startsWith(prefix);
+    }
+}
