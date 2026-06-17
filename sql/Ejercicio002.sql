a2c006946712b7c566fc9215330b4992bfd9c27a
Eder Carmona <vce00377@innovaccion.mx>
2026-06-17 14:24:02 -0600

    Stataion 17

--- sql/Ejercicio002.sql
+++ sql/Ejercicio002.sql
@@ -98,3 +98,34 @@ than  and less than . Truncate your answer to  decimal places.
 */
 select round(sum(LAT_N),4) from STATION
 where lat_n between 38.7880 AND 137.2345
+
+/*Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than .
+ Truncate your answer to  decimal places.*/
+ SELECT ROUND(MAX(LAT_N),4) FROM STATION
+WHERE LAT_N < 137.2345
+
+/*
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION 
+that is less than . Round your answer to  decimal places.
+*/
+
+SELECT ROUND(LONG_W,4) FROM STATION
+WHERE LAT_N < 137.2345
+ORDER BY LAT_N DESC
+LIMIT 1
+
+/*
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than . 
+Round your answer to  decimal places.
+*/
+ SELECT ROUND(MIN(LAT_N),4) FROM STATION
+WHERE LAT_N > 38.7780
+
+/*
+Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) 
+in STATION is greater than . Round your answer to  decimal places.
+*/
+SELECT ROUND(LONG_W,4) FROM STATION
+WHERE LAT_N > 38.7780
+ORDER BY LAT_N ASC
+LIMIT 1
