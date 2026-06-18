7b29cce44e9ded4bea5258fc75bd37966ce33a89
Eder Carmona <vce00377@innovaccion.mx>
2026-06-18 14:15:17 -0600

    reto hacker rank

--- sql/Ejercicio002.sql
+++ sql/Ejercicio002.sql
@@ -129,3 +129,35 @@ SELECT ROUND(LONG_W,4) FROM STATION
 WHERE LAT_N > 38.7780
 ORDER BY LAT_N ASC
 LIMIT 1
+
+/*
+Consider  and  to be two points on a 2D plane.
+
+ happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+ happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+ happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+ happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+*/
+SELECT ROUND(
+    ABS(MIN(LAT_N) - MAX(LAT_N)) + 
+    ABS(MIN(LONG_W) - MAX(LONG_W)), 
+    4
+) FROM STATION;
+
+/*
+Consider  and  to be two points on a 2D plane where  
+are the respective minimum and maximum values of 
+Northern Latitude (LAT_N) and  are the respective 
+minimum and maximum values of Western Longitude 
+(LONG_W) in STATION.
+
+Query the Euclidean Distance between points  and  
+and format your answer to display  decimal digits.
+*/
+
+SELECT ROUND(
+    SQRT(POW(ABS(MIN(LAT_N) - MAX(LAT_N)),2) + 
+    POW(ABS(MIN(LONG_W) - MAX(LONG_W)),2)), 
+    4
+) FROM STATION;
