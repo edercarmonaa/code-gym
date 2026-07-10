e7f9511d5241897f1c0714f4815711156a274e28
Eder Carmona <vce00377@innovaccion.mx>
2026-07-09 23:31:57 -0600

    AspectRatio

---
+++ objective-c/AspectRatio/AspectRatio/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AspectRatio
+//
+//  Created by Eder Carmona Armijo on 09/07/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
