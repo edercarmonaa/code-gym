fc1aab39f467927fea63130bc943633f7a0b07bf
Eder Carmona <vce00377@innovaccion.mx>
2026-07-13 14:10:08 -0600

    Eliminar consulta de distancia euclidiana y agregar nueva consulta para calcularla en Ejercicio023.sql

--- sql/Ejercicio002.sql
+++ sql/Ejercicio002.sql
@@ -145,19 +145,3 @@ SELECT ROUND(
     4
 ) FROM STATION;
 
-/*
-Consider  and  to be two points on a 2D plane where  
-are the respective minimum and maximum values of 
-Northern Latitude (LAT_N) and  are the respective 
-minimum and maximum values of Western Longitude 
-(LONG_W) in STATION.
-
-Query the Euclidean Distance between points  and  
-and format your answer to display  decimal digits.
-*/
-
-SELECT ROUND(
-    SQRT(POW(ABS(MIN(LAT_N) - MAX(LAT_N)),2) + 
-    POW(ABS(MIN(LONG_W) - MAX(LONG_W)),2)), 
-    4
-) FROM STATION;
